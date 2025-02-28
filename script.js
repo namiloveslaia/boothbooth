2bf5cd9d9dca4ccd1d325810f3ced7d2ca81482c
Nami <msclayfish@Gmail.com>
2025-02-28 22:38:40 +0800

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,18 @@ function takePhoto() {
     canvas.height = video.videoHeight;
     ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
 }
+const video = document.querySelector("video");
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+function drawFlippedVideo() {
+    ctx.save();
+    ctx.translate(canvas.width, 0);
+    ctx.scale(-1, 1); // Flip horizontally
+    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+    ctx.restore();
+    requestAnimationFrame(drawFlippedVideo);
+}
+
+// Start drawing when the video starts playing
+video.addEventListener("play", drawFlippedVideo);
